d2eb9d74555ef268571fdd4aa521324f589b7fae
Martin <xhrudka@gmail.com>
2022-11-23 16:43:35 +0100

    Update back_morse.py

--- back_morse.py
+++ back_morse.py
@@ -1,4 +1,7 @@
+abc = {".-":"a","-...":"b"}
 print("Hello in Morse code translater! Enter the morse code with, between the letter put: /")
+outputList_abc = []
+textoutput_abc = ()
 text = input()  
 #Text in letter
 textInLetters = text.split("/")
@@ -9,3 +12,11 @@ print(text)
 print(type(text))
 print(textInLetters)
 print(type(textInLetters))
+
+for letter_abc in textInLetters:
+    new_symbol_abc = abc.get(letter_abc)
+    outputList_abc.append(new_symbol_abc)
+
+# Join the output list to the string by map function
+textoutput_abc ='' .join(map(str,outputList_abc))
+print(textoutput_abc)
