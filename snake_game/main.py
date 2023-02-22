62c680fdbaaa33b81d3d3bbeb2b567861c7450f9
xhrudka <59419208+xhrudka@users.noreply.github.com>
2023-02-22 09:25:00 +0100

    snake game main

---
+++ snake_game/main.py
@@ -0,0 +1,38 @@
+from turtle import Screen, Turtle
+import time
+
+# Basic game settings
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+# Animation On and OFF
+screen.tracer(False)
+
+# Building of the snake's segments
+start_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+
+for position in start_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+    
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    # Time delay imported from the module time
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1): # Range function comes from C language
+        new_x = segments[seg_num - 1].xcor() 
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+        
+screen.exitonclick()
