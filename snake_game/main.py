11137428eca2f77e41ffd8674934c3bd1d21f67e
xhrudka <59419208+xhrudka@users.noreply.github.com>
2023-04-12 16:31:30 +0200

    snake_inheritance

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 # Basic game settings
@@ -11,6 +13,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,4 +27,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    #Decect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 screen.exitonclick()
