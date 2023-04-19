3b067fff07e1290831fd90163f69cdb7c4dedb77
xhrudka <59419208+xhrudka@users.noreply.github.com>
2023-04-19 15:39:35 +0200

    pong_in_progress

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,4 +1,7 @@
+import time
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,27 +9,27 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0) #Turn off of animation
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-    
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-    
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    # Detection of collision with the wall
+    if ball.ycor() > 280 or ball.ycor() <-280:
+        ball.bounce_y()
+
+    # Detection of collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
