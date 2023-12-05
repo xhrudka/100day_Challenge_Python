bd479b2efd01ddaf416da0bcca6b2c877fe17515
Martin Hrudka <martin.hrudka@buhlergroup.com>
2023-12-05 14:22:17 +0100

    Turtle_crossing_in_progress

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -17,3 +18,18 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
