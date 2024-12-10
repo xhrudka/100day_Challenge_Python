2d4e3e97f9b2ebcf66567840e78cfbb4f0ff6549
Martin <59419208+xhrudka@users.noreply.github.com>
2024-12-10 15:42:22 +0100

    pandas fun

---
+++ list_comprehension/main.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3]
+new_numbers = [n + 1 for n in numbers]
+
+print(new_numbers)
--- pandas/main.py
+++ pandas/main.py
@@ -1,14 +1,3 @@
-#import csv
-
-#with open("weather_data.csv") as data_file:
-#    data = csv.reader(data_file)
-#    temperature = []
-#    for row in data:
-#        if row[1] != "temp":
-#            temperature.append(int(row[1]))
-#    print(temperature)
-
 import pandas
-
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+#print(data["temp"])
