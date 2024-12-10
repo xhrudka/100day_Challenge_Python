933ee47d9854db6519cefa6ad45ffee4ed03b476
Martin <xhrudka@gmail.com>
2024-12-10 22:12:15 +0100

    file file open

--- pandas/main.py
+++ pandas/main.py
@@ -1,3 +1,12 @@
 import pandas
-data = pandas.read_csv("weather_data.csv")
-#print(data["temp"])
+import csv
+file_path = r"C:\Users\marti\OneDrive\Documents\GitHub\100day_Challenge_Python\pandas\weather_data.csv"
+
+with open (file_path, mode="r") as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+
+    for row in csv_reader:
+        print(row)
+#  data = pandas.read_csv("weather_data.csv")
+#print(data)
---
+++ pandas/main_file.py
@@ -0,0 +1,3 @@
+with open('file_test.txt', 'r') as file:
+    content = file.read()
+    print(content)
